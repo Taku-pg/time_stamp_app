bdfcd8a072872e9c0bf4cf63864fdfab960418fe
Taku-pg <kaitsu505@gmail.com>
2025-05-27 11:37:40 +0200

    Initialize project

---
+++ TimeStmpApp/src/main/java/org/example/timestmpapp/TimeStmpAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.timestmpapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeStmpAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeStmpAppApplication.class, args);
+    }
+
+}
---
+++ TimeStmpApp/src/test/java/org/example/timestmpapp/TimeStmpAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.timestmpapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeStmpAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
